7069d29a54e9d654149376ea70143f8fbdcc8b2e
Marty Taylor <crocojim18@gmail.com>
2020-12-26 15:11:26 -0500

    +Elizabeth township

---
+++ alleco/spiders/elizabeth_t.py
@@ -0,0 +1,38 @@
+import scrapy, re
+from alleco.objects.official import Official
+from alleco.objects.official import getAllText
+
+class elizabeth_t(scrapy.Spider):
+	name = "elizabeth_t" # name of spider
+	muniName = "ELIZABETH" # name of municipality
+	muniType = "TOWNSHIP" # type of municipality - township, borough, etc.
+	complete = True # do not change until spider is complete
+
+	def start_requests(self):
+		urls = ["https://www.elizabethtownshippa.com/board-of-commissioners",
+		"https://www.elizabethtownshippa.com/tax-collector"] # urls for requests go here
+		for url in urls:
+			yield scrapy.Request(url=url,callback=self.parse)
+
+	def parse(self, response):
+		if "tax" in response.url:
+			for quote in response.xpath("//div[p/text()='Tax Collector']/.."):
+				yield Official(
+					muniName=self.muniName,
+					muniType=self.muniType,
+					office="TAX COLLECTOR",
+					name=quote.xpath("div[2]/h1/span/span/span/text()").get(),
+					phone=quote.xpath("div[4]/p/text()").get(),					
+					url=response.url)
+		elif "commissioners" in response.url:
+			for quote in response.xpath("//p[contains(text(),'Ward ')]/../.."):
+				allText = getAllText(quote)
+				yield Official(
+					muniName=self.muniName,
+					muniType=self.muniType,
+					office="COMMISSIONER",
+					name=allText[0],
+					email=allText[1],
+					phone=allText[2],
+					district=allText[3].upper(),					
+					url=response.url)
